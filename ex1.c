d7ffafac962ae7c93297ba91d937aa42dba8d57e
Jacob Bilger <bilger.jacob@gmail.com>
2022-05-15 21:10:32 -0500

    working through it

---
+++ ex1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* This is a comment */
+int main(int argc, char *argv[])
+{
+    int distance_you = 100;
+    int distance_man = 80;
+
+    //this is also a comment
+    printf("You are %d miles away.\nThere is a man on a bridge.\n\tHe is %d miles away\n", distance_you, distance_man);
+
+    return 0;
+}
